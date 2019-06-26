37eee1226539a7984ffd844f034e9cb700dc78a7
Taran Cacacho <tarancacacho@gmail.com>
2019-06-25 23:06:41 -0700

    Create #where module instance method, and mixin to SQLObject:Class

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,20 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.keys.map {|param| "#{param.to_s} = ?"}.join(' AND ')
+    res = DBConnection.instance.execute(<<-SQL, *params.values)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_line}
+    SQL
+    res.map {|row| self.new(row)}
   end
 end
 
 class SQLObject
   # Mixin Searchable here...
+  self.extend(Searchable)
 end
